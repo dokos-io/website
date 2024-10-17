147aff8d3c2546f359699cb615169e7d49670a1e
Charles-Henri Decultot <chdecultot@dokos.io>
2024-10-17 10:23:56 +0200

    feat: Additional elements to tiers lieux

--- pages/tiers-lieux.vue
+++ pages/tiers-lieux.vue
@@ -26,59 +26,91 @@ useSeoMeta({
     <UPage>
       <UPageBody>
         <UContainer>
-          <ULandingHero :links="page.hero.links" orientation="vertical"
-      :ui="{ base: 'relative z-[1]' }" class="mb-[calc(var(--header-height)*2)]">
-      <template #headline>
-        <UBadge v-if="page.hero.headline" variant="subtle" size="lg" class="relative rounded-full font-semibold">
-          <UIcon v-if="page.hero.headline.left_icon" :name="page.hero.headline.left_icon" class="mr-1 w-4 h-4 pointer-events-none" />
-          <NuxtLink :to="page.hero.headline.to" target="_blank" class="focus:outline-none" tabindex="-1">
-            <span class="absolute inset-0" aria-hidden="true" />
-          </NuxtLink>
+          <ULandingHero :links="page.hero.links" orientation="vertical" :ui="{ base: 'relative z-[1]' }">
+            <template #headline>
+              <UBadge v-if="page.hero.headline" variant="subtle" size="lg" class="relative rounded-full font-semibold">
+                <UIcon v-if="page.hero.headline.left_icon" :name="page.hero.headline.left_icon"
+                  class="mr-1 w-4 h-4 pointer-events-none" />
+                <NuxtLink :to="page.hero.headline.to" target="_blank" class="focus:outline-none" tabindex="-1">
+                  <span class="absolute inset-0" aria-hidden="true" />
+                </NuxtLink>
 
-          {{ page.hero.headline.label }}
+                {{ page.hero.headline.label }}
 
-          <UIcon v-if="page.hero.headline.right_icon" :name="page.hero.headline.right_icon"
-            class="ml-1 w-4 h-4 pointer-events-none" />
-        </UBadge>
-      </template>
+                <UIcon v-if="page.hero.headline.right_icon" :name="page.hero.headline.right_icon"
+                  class="ml-1 w-4 h-4 pointer-events-none" />
+              </UBadge>
+            </template>
 
-      <template #title>
-        <span v-html="page.hero.title"></span>
-      </template>
+            <template #title>
+              <span v-html="page.hero.title"></span>
+            </template>
 
-      <template #description>
-        <span v-html="page.hero.description"></span>
-      </template>
+            <template #description>
+              <span v-html="page.hero.description"></span>
+            </template>
 
-      <template #default>
-        <NuxtImg
-          :src="'/home/' + page.hero.image"
-          class="w-full rounded-md bg-white/75"
-          v-if="page.hero.image"
-        />
-        <Placeholder v-else />
+            <template #default>
+              <NuxtImg :src="'/home/' + page.hero.image" class="w-full rounded-md bg-white/75" v-if="page.hero.image" />
+              <Placeholder v-else />
 
-      </template>
-    </ULandingHero>
+            </template>
+          </ULandingHero>
 
-            <!-- <UPageGrid :ui="{'wrapper': 'grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-2 gap-8'}">
-              <UCard v-for="(card, index) in page.cards" :key="index" v-bind="card" target="_blank" :ui="{
-                header: { base: card.color + ' rounded-t-xl'},
-                base: 'text-center'
-              }">
-                <template #header>
-                  <p class="text-gray-900 dark:text-white text-base font-semibold truncate gap-1.5">{{  card.title }}</p>
-                </template>
+          <ULandingSection>
+            <ULandingLogos :title="page.logos.title" :ui="{ images: 'justify-center' }">
+              <UIcon v-for="icon in page.logos.icons" :key="icon" :name="icon"
+                class="w-16 h-16 flex-shrink-0 text-gray-500 dark:text-gray-400" />
+              <NuxtImg v-for="image in page.logos.images" :key="image" :src="image" class="max-h-20" />
+            </ULandingLogos>
+          </ULandingSection>
 
-                <template #footer>
-                    <div class="text-[15px] text-gray-500 dark:text-gray-400 mt-1 mb-6">{{  card.description }}</div>
-                    <UButton v-bind="card.button" />
-                </template>
-              </UCard>
-            </UPageGrid> -->
+          <ULandingSection v-bind="page.colored_section" v-if="page.colored_section"
+            :ui="{ container: 'bg-blue-950 rounded-3xl p-5', title: 'mt-2 text-orange-200', description: 'text-white' }">
+            <template #headline>
+              <UBadge v-if="page.colored_section.badge" :variant="page.colored_section.badge.variant || 'subtle'"
+                :size="page.colored_section.badge.size || 'lg'" :color="page.colored_section.badge.color || 'primary'"
+                class="relative rounded-full font-semibold">
+                <UIcon v-if="page.colored_section.badge.left_icon" :name="page.colored_section.badge.left_icon"
+                  class="mr-1 w-4 h-4 pointer-events-none" />
+                <NuxtLink :to="page.colored_section.badge.to" target="_blank" class="focus:outline-none" tabindex="-1">
+                  <span class="absolute inset-0" aria-hidden="true" />
+                </NuxtLink>
 
-          <ULandingSection :v-bind="page.faq" v-if="page.faq">
-            <ULandingFAQ :items="page.faq.items" multiple :class="page.faq" />
+                {{ page.colored_section.badge.label }}
+
+                <UIcon v-if="page.colored_section.badge.right_icon" :name="page.colored_section.badge.right_icon"
+                  class="ml-1 w-4 h-4 pointer-events-none" />
+              </UBadge>
+            </template>
+
+            <div class="py-8 rounded-lg"
+              :class="[page.colored_section.image_bg_color, page.colored_section.align == 'left' ? 'pl-8' : 'pr-8']"
+              v-if="page.colored_section.image">
+              <NuxtImg :src="page.colored_section.image" class="shadow-lg"
+                :class="page.colored_section.align == 'left' ? 'rounded-s-lg' : 'rounded-e-lg'" />
+            </div>
+            <Placeholder v-else class="bg-orange-200" />
+          </ULandingSection>
+
+
+          <ULandingSection v-for="(section, index) in page.sections" :key="index" v-bind="section">
+            <template #title>
+              <span v-html="section.title" />
+            </template>
+            <template #description>
+              <span v-html="section.description" />
+            </template>
+            <div class="py-8 rounded-lg" :class="[section.image_bg_color, section.align == 'left' ? 'pl-8' : 'pr-8']"
+              v-if="section.image">
+              <NuxtImg :src="section.image" class="shadow-lg"
+                :class="section.align == 'left' ? 'rounded-s-lg' : 'rounded-e-lg'" />
+            </div>
+            <Placeholder v-else />
+          </ULandingSection>
+
+          <ULandingSection>
+            <ULandingCTA v-bind="page.cta" class="bg-orange-100/50 dark:bg-orange-800/50" />
           </ULandingSection>
         </UContainer>
       </UPageBody>
